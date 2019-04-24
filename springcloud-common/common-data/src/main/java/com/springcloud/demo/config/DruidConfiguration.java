c538f106552d53051aa9176566ab70fcec71d333
lai.guanfu <lai.guanfu@trs.com.cn>
2019-04-24 15:04:10 +0800

    1.更换spring boot整合Druid的依赖包为druid-spring-boot-starter，且上升版本为1.1.10 2.配置druid数据连接池的基本参数 3.增加DruidConfiguration配置类，实现Druid的数据监控 4.增加必要注释

---
+++ springcloud-common/common-data/src/main/java/com/springcloud/demo/config/DruidConfiguration.java
@@ -0,0 +1,50 @@
+package com.springcloud.demo.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description Druid数据源的配置
+ * @auther: lai.guanfu
+ * @date: 2019-04-24 11:43
+ */
+@Configuration
+public class DruidConfiguration {
+
+    /**
+     * 注册Servlet信息， 配置监控视图
+     */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<String, String>();
+        initParameters.put("loginUsername", "admin");// 用户名
+        initParameters.put("loginPassword", "admin");// 密码
+        initParameters.put("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
+        initParameters.put("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
+        //initParameters.put("deny", "192.168.20.38");// IP黑名单 (存在共同时，deny优先于allow),如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册Filter信息, 监控拦截器
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
